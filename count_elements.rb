1da63afc1ccdd156405ae778e6a7f5d6cfa0f308
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-30 23:36:38 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,6 +2,6 @@ require "pry"
 
 def count_elements(array)
   # code goes here
-  return Hash[array.group_by {|x| x}.collect {|name,count| name,name.count}]
+  return Hash[array.group_by {|x| x}.collect {|name,number| [name,number.count]}]
 
 end
